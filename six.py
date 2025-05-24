2ae22b194b9bbfaf4d3a0dffedbb07f552e27bcd
GANESH_PAULRAJA <ganeshsgk555@gmail.com>
2025-05-24 16:40:43 +0530

    scipy new unites

--- six.py
+++ six.py
@@ -33,5 +33,79 @@
 
 # print("Size is: ", byt)
 
-# -----------------------------------------
+# ---------------------------------------
 
+# Working with degree of angle
+
+# from scipy import constants
+
+# print(constants.degree) #converts to radian
+
+# deg = int(input('Enter the degree of angle'))
+
+# rad = constants.degree * deg
+
+# print('Degree in radian: ', rad)
+
+
+# ----------------------------------------------
+
+# from scipy import constants
+
+# print(constants.gram) #always return kg
+
+# wet = int(input("Enter the weight in gram"))
+
+# kgm = constants.gram * wet
+
+# print("Weight in kilo gram: ", kgm)
+
+# ---------------------------------------------
+
+# from scipy import constants
+
+# print(constants.minute) #always return kg
+
+# min = int(input("Enter the time in minute"))
+
+# kgm = constants.minute * min
+
+# print("Time in seconds: ", kgm)
+# ------------------------------------------------
+
+# from scipy import constants
+
+# print(constants.inch) #always return mtr
+
+# inc = int(input("Enter the length"))
+
+# mtr = constants.inch * inc
+
+# print("Length in meter: ", mtr)
+# --------------------------------------------
+
+# working with pressure
+
+# from scipy import constants
+
+# print(constants.bar) #always return mtr
+
+# pre = int(input("Enter the pressure in bar"))
+
+# pac = constants.bar * pre
+
+# print("Pressure in pascal : ", pac)
+
+# -------------------------------------------------
+
+# working with Area
+
+from scipy import constants
+
+print(constants.acre) 
+
+acr = int(input("Enter the area in acre"))
+
+sqm = constants.acre * acr
+
+print("Area in sqm: ", sqm)
